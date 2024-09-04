c05999791a147ba9366a955e439c4d21e4a2d3f9
CharithaGundala <charithagundala2004@gmail.com>
2024-09-04 09:48:17 +0530

    Altered maps

--- mine/views.py
+++ mine/views.py
@@ -214,7 +214,7 @@ def profile(request):
     return render(request, 'profile.html', {'user': user})
 
 def admindash(request):
-    mines = mineDetail.objects.exclude(id=1)
+    mines = mineDetail.objects.exclude(username = "ministryofcoal")
     details = []
     for mine in mines:
         address = mineaddress.objects.get(username=mine).state
